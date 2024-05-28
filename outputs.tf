43f26c84316251cac09d3066c9bf04ceb5a2e2ea
Alexandre Rabello <alerabello95@hotmail.com>
2024-05-28 15:08:57 -0300

    update (Outputs): correção value

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "cluster_ca_certificate_authority_data" {
     description = "base64 encoded PEM certificate of the CA that signed the EKS cluster certificate"
-    value = try(aws_eks_cluster.this[0].certificate_authority[0]data, null)
+    value = try(aws_eks_cluster.this[0].certificate_authority[0].data, null)
 }
